33bc92f0323f2bdb241ee53578e89abc041c5cab
chuchulu29117 <chuchulu117@gmail.com>
2024-11-01 14:30:25 +0200

    files added

--- Libft/ft_strdup.c
+++ Libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: clu <clu@student.hive.fi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 12:00:47 by clu               #+#    #+#             */
-/*   Updated: 2024/11/01 12:05:14 by clu              ###   ########.fr       */
+/*   Updated: 2024/11/01 14:28:42 by clu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ char	*ft_strdup(const char *s1)
 
 	len = ft_strlen(s1) + 1;
 	ptr = (char *)malloc(len);
-	if (ptr == 0)
-		return (0);
+	if (ptr == NULL)
+		return (NULL);
 	ft_memcpy(ptr, s1, len);
 	return (ptr);
 }
