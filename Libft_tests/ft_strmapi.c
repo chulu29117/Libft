f2cb96cec1c26c6c30d95e97506591da2071f6df
clu <clu@student.hive.fi>
2024-11-05 20:36:26 +0200

    files added

--- Libft_tests/ft_strmapi.c
+++ Libft_tests/ft_strmapi.c
@@ -6,13 +6,61 @@
 /*   By: clu <clu@student.hive.fi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 16:26:57 by clu               #+#    #+#             */
-/*   Updated: 2024/11/05 16:27:21 by clu              ###   ########.fr       */
+/*   Updated: 2024/11/05 20:35:45 by clu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+size_t	ft_strlen(const char *str);
+
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	
-}	
+	char	*new;
+	size_t	i;
+
+	if (!s || !f)
+		return (NULL);
+	new = (char *)malloc(ft_strlen(s) + 1);
+	if (!new)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		new[i] = f(i, s[i]);
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
+}
+
+// Test for ft_strmapi
+#include <stdio.h>
+
+char	ft_example(unsigned int i, char c)
+{
+	return (c + i);
+}
+
+int	main(void)
+{
+	char	*str = "ABCD";
+	char	*new;
+
+	new = ft_strmapi(str, ft_example);
+	printf("Original: %s\n", str);
+	printf("New: %s\n", new);
+	free(new);
+	return (0);
+}
+
+/// Helper function ft_strlen ///
+size_t	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
