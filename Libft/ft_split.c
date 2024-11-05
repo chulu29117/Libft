801ce9014eb4f746273481667a870e54a406705d
chuchulu29117 <chuchulu117@gmail.com>
2024-11-05 15:45:31 +0200

    files added

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,11 +6,31 @@
 /*   By: clu <clu@student.hive.fi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 20:51:38 by clu               #+#    #+#             */
-/*   Updated: 2024/11/05 10:47:28 by clu              ###   ########.fr       */
+/*   Updated: 2024/11/05 15:39:36 by clu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static size_t	count_words(char const *s, char c);
+static char		*get_next_word(char const *s, char c, size_t *i);
+static void		free_split(char **split, size_t j);
+static void		write_split(char **split, char const *s, char c);
+
+char	**ft_split(char const *s, char c)
+{
+	char	**split;
+	size_t	words;
+
+	if (!s)
+		return (NULL);
+	words = count_words(s, c);
+	split = (char **)malloc(sizeof(char *) * (words + 1));
+	if (!split)
+		return (NULL);
+	write_split(split, s, c);
+	return (split);
+}
 
 static size_t	count_words(char const *s, char c)
 {
@@ -50,19 +70,24 @@ static char	*get_next_word(char const *s, char c, size_t *i)
 	return (word);
 }
 
-char	**ft_split(char const *s, char c)
+static void	free_split(char **split, size_t j)
 {
-	char	**split;
-	size_t	words;
+	size_t	i;
+
+	i = 0;
+	while (i < j)
+	{
+		free(split[i]);
+		i++;
+	}
+	free(split);
+}
+
+static void	write_split(char **split, char const *s, char c)
+{
 	size_t	i;
 	size_t	j;
 
-	if (!s)
-		return (NULL);
-	words = count_words(s, c);
-	split = (char **)malloc(sizeof(char *) * (words + 1));
-	if (!split)
-		return (NULL);
 	i = 0;
 	j = 0;
 	while (s[i])
@@ -70,11 +95,12 @@ char	**ft_split(char const *s, char c)
 		if (s[i] != c)
 		{
 			split[j] = get_next_word(s, c, &i);
+			if (!split[j])
+				free_split(split, j);
 			j++;
 		}
 		else
 			i++;
 	}
 	split[j] = NULL;
-	return (split);
 }
--- Libft_tests/ft_split.c
+++ Libft_tests/ft_split.c
@@ -6,12 +6,64 @@
 /*   By: clu <clu@student.hive.fi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 20:53:40 by clu               #+#    #+#             */
-/*   Updated: 2024/11/05 11:58:07 by clu              ###   ########.fr       */
+/*   Updated: 2024/11/05 15:44:23 by clu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+size_t	ft_strlen(const char *str);
+char	*ft_strdup(const char *s1);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+static size_t	count_words(char const *s, char c);
+static char		*get_next_word(char const *s, char c, size_t *i);
+static void		free_split(char **split, size_t j);
+static void		write_split(char **split, char const *s, char c);
+
+char	**ft_split(char const *s, char c)
+{
+	char	**split;
+	size_t	words;
+
+	if (!s)
+		return (NULL);
+	words = count_words(s, c);
+	split = (char **)malloc(sizeof(char *) * (words + 1));
+	if (!split)
+		return (NULL);
+	write_split(split, s, c);
+	return (split);
+}
+
+// Test for ft_split
+#include <stdio.h>
+
+int	main(void)
+{
+	char	**split;
+	char	**split_1;
+
+	int		i;
+
+	split = ft_split("Hello Good Morning World This is a Test", ' ');
+	split_1 = ft_split("Hello,Good,Morning,World,This,is,a,Test", ',');
+	i = 0;
+	while (split[i])
+	{
+		printf("%s\n", split[i]);
+		i++;
+	}
+	printf("\n");
+	i = 0;
+	while (split_1[i])
+	{
+		printf("%s\n", split_1[i]);
+		i++;
+	}
+}
+
+/// Helper functions ///
 size_t	ft_strlen(const char *str)
 {
 	int	i;
@@ -54,6 +106,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	return (dest);
 }
 
+// Function to extract a substring from the string s starting at index start and of length len
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*substr;
@@ -123,78 +176,39 @@ static char	*get_next_word(char const *s, char c, size_t *i)
 	return (word);
 }
 
-char	**ft_split(char const *s, char c)
+// Function to free the memory allocated for the split array
+static void	free_split(char **split, size_t j)
 {
-	char	**split; 	// Array of strings to store the words
-	size_t	words;
-	size_t	i;			// Index of the current character in the string
-	size_t	j;			// Index of the current word in the array
+	size_t	i;
 
-	if (!s)
-		return (NULL);
-	// Count the number of words in the string
-	words = count_words(s, c);
-	// Allocate memory for the array of strings
-	split = (char **)malloc(sizeof(char *) * (words + 1));
-	if (!split)
-		return (NULL);
 	i = 0;
-	j = 0;
-	// Extract the words from the string
-	while (s[i])
-	{
-		if (s[i] != c)
-		{
-			split[j] = get_next_word(s, c, &i); // Extract the next word
-			j++;
-		}
-		else	// Skip the delimiter
-			i++;
-	}
-	split[j] = NULL; // Set the last element of the array
-	return (split);
-}
-
-// Test for ft_split
-#include <stdio.h>
-
-int	main(void)
-{
-	char	**split;
-	char	**split_1;
-
-	int		i;
-
-	split = ft_split("Hello Good Morning World This is a Test", ' ');
-	split_1 = ft_split("Hello,Good,Morning,World,This,is,a,Test", ',');
-	i = 0;
-	while (split[i])
-	{
-		printf("%s\n", split[i]);
-		i++;
-	}
-	printf("\n");
-	i = 0;
-	while (split_1[i])
-	{
-		printf("%s\n", split_1[i]);
-		i++;
-	}
-	// Free the memory allocated
-	i = 0;
-	while (split[i])
+	while (i < j)
 	{
 		free(split[i]);
 		i++;
 	}
 	free(split);
-	// Free the memory allocated
+}
+
+// Function to write the words to the split array
+static void	write_split(char **split, char const *s, char c)
+{
+	size_t	i;
+	size_t	j;
+
 	i = 0;
-	while (split_1[i])
+	j = 0;
+	while (s[i])
 	{
-		free(split_1[i]);
-		i++;
+		if (s[i] != c)
+		{
+			split[j] = get_next_word(s, c, &i);
+			if (!split[j])
+				free_split(split, j);
+			j++;
+		}
+		else
+			i++;
 	}
-	free(split_1);
-	return (0);
+	split[j] = NULL;
 }
